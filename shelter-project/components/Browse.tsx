b2eea0b67b41e528a6b146b08b5ec4d75125e48d
tatiana-stewart-mck <tatiana_stewart@mckinsey.com>
2025-01-30 10:44:59 -0500

    Styled Browse page

--- shelter-project/components/Browse.tsx
+++ shelter-project/components/Browse.tsx
@@ -1,60 +1,96 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Button } from "react-native";
-import { SearchBar } from "react-native-elements";
+import { StyleSheet, View, Text } from "react-native";
+import { SearchBar, Button, Icon } from "react-native-elements";
 
 import ShelterCard from "./ShelterCard";
-import { useApi } from '../app/ApiContext'
-import { Colors } from "../constants/Colors"
+import { useApi } from "../app/ApiContext";
+import { Colors } from "../constants/Colors";
 import { Spacing } from "@/constants/Spacing";
- 
+import { HeaderTitle } from "@react-navigation/elements";
+import { Fonts } from "@/constants/Fonts";
 
 export default function Browse() {
-    const { allShelters } = useApi();
-    const [search, setSearch] = useState();
-    const [filteredshelters, setFilteredShelters] = useState(allShelters);
+  const { allShelters } = useApi();
+  const [search, setSearch] = useState();
+  const [filteredshelters, setFilteredShelters] = useState(allShelters);
 
-    const filterShelters = (query: any) => {
-        const lowercaseQuery = query.toLowerCase();
-        const filltered = allShelters.filter(shelter =>
-            shelter.shelter_name.toLowerCase().includes(lowercaseQuery) ||
-            shelter.address.toLowerCase().includes(lowercaseQuery)
-        );
-        setFilteredShelters(filltered)
-    };
-    
-      const updateSearch = (search: any) => {
-        setSearch(search);
-        filterShelters(search);
-      };
-    
-    return (
-        <>
-            <SearchBar 
-                placeholder="Search Shelters..."
-                platform="ios"
-                onChangeText={updateSearch}
-                value={search}
-                containerStyle={Spacing.searchBarContainer}
-                inputContainerStyle={Spacing.searchBarInputContainer}
-                searchIcon={{ name: "search", type: 'ionicon', size: 24, color: Colors.light.icon}}
-            />
+  const filterShelters = (query: any) => {
+    const lowercaseQuery = query.toLowerCase();
+    const filltered = allShelters.filter(
+      (shelter) =>
+        shelter.shelter_name.toLowerCase().includes(lowercaseQuery) ||
+        shelter.address.toLowerCase().includes(lowercaseQuery)
+    );
+    setFilteredShelters(filltered);
+  };
 
-            <View style={styles.buttoncontainer}>
-                <Button title="List View"></Button>
-                <Button title="Map View"></Button>
-            </View>
+  const updateSearch = (search: any) => {
+    setSearch(search);
+    filterShelters(search);
+  };
 
-            <ShelterCard shelters={filteredshelters}/>
-        </>
-    )
+  return (
+    <View style={styles.mainContainer}>
+      <SearchBar
+        placeholder="Search Shelters..."
+        platform="ios"
+        onChangeText={updateSearch}
+        value={search}
+        containerStyle={[Spacing.searchBarContainer, {marginLeft: 20, marginRight: 20}]}
+        inputContainerStyle={Spacing.searchBarInputContainer}
+        searchIcon={{
+          name: "search",
+          type: "ionicon",
+          size: 24,
+          color: Colors.light.icon,
+        }}
+      />
+
+      <View style={styles.buttoncontainer}>
+        <Button
+          icon={<Icon name="list" color="white" />}
+          buttonStyle={styles.viewTypeButton}
+        ></Button>
+        <Button
+          icon={<Icon name="map" color="white" />}
+          buttonStyle={styles.viewTypeButton}
+        ></Button>
+      </View>
+
+      <View style={styles.backgroundContainer}>
+        <HeaderTitle style={[Fonts.sectionTitle, styles.sectionTitle]}>
+          Shelters
+        </HeaderTitle>
+        <Text style={{ fontWeight: 500, marginLeft: 20 }}>
+          Result(s): {filteredshelters.length}
+        </Text>
+        <ShelterCard shelters={filteredshelters} />
+      </View>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    buttoncontainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        marginVertical: 10
-    }
-  });
-  
+  mainContainer: {
+    backgroundColor: "black",
+    paddingTop: 20,
+  },
+  buttoncontainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 10,
+    marginVertical: 10,
+  },
+  backgroundContainer: {
+    backgroundColor: Colors.light.tint,
+    height: "100%",
+    borderTopStartRadius: 25,
+    borderTopEndRadius: 25,
+  },
+  sectionTitle: {
+    marginTop: 20,
+  },
+  viewTypeButton: {
+    backgroundColor: "none",
+  },
+});
